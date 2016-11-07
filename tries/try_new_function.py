d5893e97c7613bcbbaa83155577f79d378efc6d4
Moritz Meier <moritzmeier@aol.com>
2016-11-07 11:49:20 +0100

    move notebooks

--- tries/try_new_function.py
+++ tries/try_new_function.py
@@ -1,24 +1,13 @@
+from treefarm.core.display_dot import to_dot
+from treefarm.core.parameters import *
+from treefarm.core.domains import *
 
 
+k = Categorical({'a','b'})
+n = Discrete(N[0:])
+r = Continuous(R[0:9])
+G = r + n | k
+f = join(add, mul)
+call = Apply(f, ParameterList(G, {}))
 
-
-class A:
-    def __new__(cls, arg):
-        print('A.new(%s, %s)' % (cls,arg))
-        return object.__new__(B)
-
-    def __init__(self):
-        print('A.init(%s)' % self)
-
-
-class B(A):
-    #def __new__(cls):
-    #    print('B.new(%s)' % cls)
-    #    return object.__new__(cls)
-
-    def __init__(self, arg):
-        print('B.init(%s, %s)' % (self,arg))
-
-a = A('foo')
-
-print(a, type(a))
+to_dot(call)
