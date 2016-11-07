6e8a7d25a720ed7793373fdfa44189ae322d7859
Moritz Meier <moritzmeier@aol.com>
2016-11-07 11:49:54 +0100

    execute new style functrees

--- treefarm/core/computing_engine.py
+++ treefarm/core/computing_engine.py
@@ -1,40 +1,26 @@
-from .domains import Call, Product
+from .domains import ParameterList
+from .parameters import Apply
 
 class ComputingEngine:
     def __init__(self, cores):
         self.cores = cores
 
-    def evaluate_func(self, function, parameters):
-        if type(parameters) == Product:
-            result = function(*parameters.args, **parameters.kwargs)
-        elif type(parameters) in (list, tuple):
-            result = function(*parameters)
-        elif type(parameters) == dict:
-            result = function(**parameters)
-        else:
-            result = function(parameters)
-        return result
-
     def evaluate(self, func_tree):
         # assume simple operators for now
 
-        if isinstance(func_tree, Product):
-            list_result = [
-                self.evaluate(sub)
-                for sub in func_tree.args]
-            dict_result = {
-                key:self.evaluate(val)
-                for key, val in func_tree.kwargs.items()}
-            domain_result = Product(list_result, dict_result)
-        else:
-            domain_result = func_tree
+        if not type(func_tree) is Apply:
+            return func_tree
 
-        if type(func_tree) == Call:
-            result = self.evaluate_func(
-                function = func_tree.operator.func,
-                parameters = domain_result)
-        else:
-            result = domain_result
+        list_result = [
+            self.evaluate(sub)
+            for sub in func_tree.domain.args]
+        dict_result = {
+            key:self.evaluate(val)
+            for key, val in func_tree.domain.kwargs.items()}
+
+        result = func_tree.operation.func(
+            *list_result,
+            **dict_result)
 
         return result
 
