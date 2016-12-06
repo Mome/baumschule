457ddf800e77c62898c353e7a474814681581afe
Moritz Meier <moritzmeier@aol.com>
2016-12-06 23:26:39 +0100

    reviving data access

---
+++ treefarm/data_access/data_access_config.py
@@ -0,0 +1,40 @@
+import sys, os
+sys.path.append(os.path.expanduser('~/code/treefarm'))
+
+c.Dataset.source_classes = {
+}
+
+# for local file access
+c.Dataset.directory_meta_filenames = ['meta', 'info', '.meta', '.info']
+c.Dataset.directory_meta_formats = ['application/json']
+
+c.Dataset.local_source_basepaths = {
+    'local' : join(c.basepath, 'datasets')
+}
+
+c.Dataset.local_default_source = 'local'
+
+c.Dataset.local_group_classes = {
+}
+
+import pandas, numpy, scipy
+
+# maps from a mime type to function
+# that reads a file on the local file system
+c.Dataset.local_read_functions = {
+    'text/csv' : pandas.read_csv,
+    'text/tab-separated-values' : pandas.read_table,
+    'application/x-numpy-data' : numpy.load,
+    'application/x-matlab-data' : scipy.io.loadmat,
+}
+
+
+def df_to_csv(path, df):
+	import pandas
+	pandas.DataFrame.to_csv(df, path)
+
+# maps from a object type to write function
+c.Dataset.local_write_functions = {
+    numpy.ndarray : numpy.save,
+    pandas.DataFrame : df_to_csv,
+}
